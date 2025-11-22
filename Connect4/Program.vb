fb0057431893a0d7723a97574dce29d74cf2b437
JustinJumbo <justin29999personal@gmail.com>
2025-11-22 01:25:47 +0000

    - Finished Project - Refactored Code

--- Connect4/Program.vb
+++ Connect4/Program.vb
@@ -8,21 +8,22 @@ Module Program
     Dim P2Name As String = ""
     Dim P1Symbol As String = ""
     Dim P2Symbol As String = ""
-    Dim GameWon As Boolean = False
 
     Sub Main()
 
+        Console.Title = "Connect 4"
+        Console.CursorVisible = False
         SetUpBoard()
         Welcome()
 
         Dim Running As Boolean = True
         While Running
 
+            Console.Clear()
             DisplayMenu()
 
             Select Case Console.ReadKey().KeyChar
-                'Play Game Selected
-                Case "1"
+                Case "1" 'Play Game Selected
                     PlayGame()
 
                     'Play Computer Selected
@@ -49,19 +50,20 @@ Module Program
 
     Sub Welcome()
 
-        Console.Write("Enter the name of Player 1: ")
+        'Welcoming Players and getting their names
+        Console.Write("Enter the name of Player 1: " & Environment.NewLine & "> ")
         P1Name = Console.ReadLine()
-        Console.Write("Enter the name of Player 2: ")
+        Console.Write("Enter the name of Player 2: " & Environment.NewLine & "> ")
         P2Name = Console.ReadLine()
 
         Console.WriteLine($"{P1Name} and {P2Name}, Welcome to Connect 4!")
         Threading.Thread.Sleep(1000)
-
         Console.Clear()
-
     End Sub
 
     Sub InvalidInput()
+
+        'Handles invalid input
         Console.WriteLine(Environment.NewLine & "Invalid Input. Try again!")
         Threading.Thread.Sleep(1000)
         Console.Clear()
@@ -69,47 +71,63 @@ Module Program
 
     Sub DisplayMenu()
 
+        'Displays Main Menu
         Console.Write("Options:         " & Environment.NewLine &
                       "-----------------" & Environment.NewLine &
                       "[1] Play Game    " & Environment.NewLine &
                       "[2] Play Computer" & Environment.NewLine &
                       "[3] Exit Game    " & Environment.NewLine &
-                      "Enter your option: ")
+                      "Enter your option: " & Environment.NewLine & "> ")
 
     End Sub
+
     Sub SetUpBoard()
 
+        'Initializes the board
         For x As Integer = 0 To 5
             For y As Integer = 0 To 6
                 Board(x, y) = "."
             Next
         Next
-
     End Sub
 
     Sub DisplayBoard()
 
-        Console.WriteLine("  1 2 3 4 5 6 7")
+        'Displays the board
+        Console.WriteLine(" 1 2 3 4 5 6 7")
         For x As Integer = 0 To 5
-            Console.Write(x & "|")
+            'Console.Write(x & "|") TEMPORARY ROW NUMBERING
+            Console.Write("|")
             For y As Integer = 0 To 6
-                Console.Write(Board(x, y) & "|")
-            Next
-            Console.WriteLine()
-        Next
 
+                'Coloring the symbols
+                If Board(x, y) = "X" Then
+                    Console.ForegroundColor = ConsoleColor.Red
+                ElseIf Board(x, y) = "O" Then
+                    Console.ForegroundColor = ConsoleColor.Cyan
+                End If
+
+                'Once colour is determined, print each symbol
+                Console.Write(Board(x, y))
+                Console.ForegroundColor = ConsoleColor.White 'Must reset colour after each symbol before printing border
+                Console.Write("|")
+
+            Next
+            Console.WriteLine() 'Makes new line after each row
+        Next
     End Sub
 
     Sub SelectSymbols()
         Dim Choice As String
 
-        'Validation for correct symbol.
+        'Validation for correct symbol
         While Not (P1Symbol = "O" Or P1Symbol = "X")
-            Console.WriteLine("Enter either O or X for the symbol!")
 
-            Console.Write($"Enter the symbol you want to assign for Player 1 ({P1Name}): ")
+            Console.WriteLine("Enter either O or X for the symbol!")
+            Console.Write($"Enter the symbol you want to assign for Player 1 ({P1Name}): " & Environment.NewLine & "> ")
             Choice = Console.ReadKey().KeyChar.ToString.ToUpper()
 
+            'Assigning symbols based on choice
             Select Case Choice
                 Case "O"
                     P1Symbol = Choice
@@ -120,61 +138,76 @@ Module Program
                 Case Else
                     InvalidInput()
             End Select
-
         End While
 
         'Displays which player has what symbol
-        Console.WriteLine(Environment.NewLine & $"Player 1 ({P1Name}) symbol: {P1Symbol}")
+        Console.Clear()
+        Console.WriteLine($"Player 1 ({P1Name}) symbol: {P1Symbol}")
         Console.WriteLine($"Player 2 ({P2Name}) symbol: {P2Symbol}")
 
         Console.Write("Enter any key to continue: ")
         Console.ReadKey()
-        Console.Clear()
 
     End Sub
 
-
     Sub PlayGame()
+        Dim Turns As Integer = 0
+        Dim GameEnd As Boolean = False
 
+        'Main game runthrough loop
         Console.Clear()
         SelectSymbols()
 
         'Displays Board, waits for user to begin
         Console.Clear()
         DisplayBoard()
+
         Console.Write("Ready to play? (Press any key to continue) : ")
         Console.ReadKey()
 
-        'While GameWon = False
+        'Loop for each turn. Continues until someone wins
+        While GameEnd = False
 
+            If Turns Mod 2 = 0 Then
+                'Player 1 Turn
+                PlaceSymbol(P1Symbol)
+                CheckWin(P1Symbol, GameEnd)
+            Else
+                'Player 2 Turn
+                PlaceSymbol(P2Symbol)
+                CheckWin(P2Symbol, GameEnd)
+            End If
 
-        'Test
-        PlaceSymbol(P1Symbol)
-        DisplayBoard()
+            Turns += 1 'Increment turn counter
 
-
-
+        End While
 
     End Sub
 
     Sub PlaceSymbol(ByVal Symbol As String)
         Dim Column As Integer
 
+        'Input Loop with Validation
         While True
-
             Console.Clear()
-            Console.Write("Enter the Column Number:")
-            Column = Console.ReadLine() - 1
+            DisplayBoard()
+            Console.WriteLine($"{If(Symbol = P1Symbol, P1Name, P2Name)}'s Turn ({Symbol})") 'result = If(condition, valueIfTrue, valueIfFalse)
+            Console.Write("Enter the Column Number:" & Environment.NewLine & "> ")
 
-            If Board(0, Column) <> "." Then
-                Console.WriteLine("Coordinate taken! Try again")
+            Dim keyChar As Char = Console.ReadKey().KeyChar
+
+            ' Try to convert key to a string, then to a number
+            If Integer.TryParse(keyChar.ToString(), Column) Then
+                Column -= 1 'converts to 0-based index
+            End If
+
+            If Column < 0 Or Column > 6 Then 'If out of bounds
+                Console.WriteLine(Environment.NewLine & "Out of Bounds! Try again")
                 Threading.Thread.Sleep(1000)
-                Console.ReadLine()
-            ElseIf Column < 1 Or Column > 7 Then
-                Console.WriteLine("Out of Bounds! Try again")
+            ElseIf Board(0, Column) <> "." Then 'If column is full
+                Console.WriteLine(Environment.NewLine & "Coordinate taken! Try again")
                 Threading.Thread.Sleep(1000)
-                Console.ReadLine()
-            Else
+            Else 'Input is valid
                 Exit While
             End If
 
@@ -182,10 +215,99 @@ Module Program
 
         'After input passes valdation, place on board
         Board(0, Column) = Symbol
+        Console.Clear()
 
-        'If Board(0, Column) Then
+        'Once placed, animate the symbol falling
+        SymbolAnimation(Symbol, Column)
+
+        DisplayBoard()
+        Threading.Thread.Sleep(200)
 
     End Sub
 
+    Sub SymbolAnimation(ByVal Symbol As String, ByVal Column As Integer)
+
+        'Animates the symbol falling
+        For x As Integer = 0 To 4 Step 1
+
+            DisplayBoard() 'Display current board state
+            Threading.Thread.Sleep(75)
+
+            If Board(x + 1, Column) = "." Then
+                Board(x + 1, Column) = Symbol 'Update position into board
+                Console.ForegroundColor = ConsoleColor.White 'Console color reset
+                Board(x, Column) = "."
+            Else
+                Console.Clear()
+                Exit For
+            End If
+
+            Console.Clear()
+
+        Next
+
+    End Sub
+
+    Sub CheckWin(ByVal Symbol As String, ByRef GameEnd As Boolean)
+
+        Dim GameWin As Boolean = False
+        Dim SpacesLeft As Integer = 0
+
+        'Check 4 Continuous Vertical Lines
+        For x As Integer = 0 To 2
+            For y As Integer = 0 To 6
+                If Board(x, y) = Symbol And Board(x + 1, y) = Symbol And Board(x + 2, y) = Symbol And Board(x + 3, y) = Symbol Then
+                    GameWin = True
+                End If
+            Next
+        Next
+        'Check 4 Continuous Horizontal Lines
+        For x As Integer = 0 To 5
+            For y As Integer = 0 To 2
+                If Board(x, y) = Symbol And Board(x, y + 1) = Symbol And Board(x, y + 2) = Symbol And Board(x, y + 3) = Symbol Then
+                    GameWin = True
+                End If
+            Next
+        Next
+        'Check 4 Leading Diagonal Lines (Top Left to Bottom Right)
+        For x As Integer = 0 To 2
+            For y As Integer = 0 To 3
+                If Board(x, y) = Symbol And Board(x + 1, y + 1) = Symbol And Board(x + 2, y + 2) = Symbol And Board(x + 3, y + 3) = Symbol Then
+                    GameWin = True
+                End If
+            Next
+        Next
+        'Check 4 Following Diagonal Lines (Top Right to Bottom Left)
+        For x As Integer = 3 To 5
+            For y As Integer = 0 To 3
+                If Board(x, y) = Symbol And Board(x - 1, y + 1) = Symbol And Board(x - 2, y + 2) = Symbol And Board(x - 3, y + 3) = Symbol Then
+                    GameWin = True
+                End If
+            Next
+        Next
+
+        'Check Draw
+        For x As Integer = 0 To 5
+            For y As Integer = 0 To 6
+                If Board(x, y) = "." Then
+                    SpacesLeft += 1
+                End If
+            Next
+        Next
+
+        'Message Display for Win or Draw
+        If GameWin = True Then
+            Console.WriteLine(Environment.NewLine & $"{If(Symbol = P1Symbol, P1Name, P2Name)} Wins! Congratulations!")
+            GameEnd = True
+            Threading.Thread.Sleep(3000)
+            Console.Clear()
+        ElseIf SpacesLeft = 0 Then
+            Console.WriteLine(Environment.NewLine & "It's a Draw! Well played both!")
+            GameEnd = True
+            Threading.Thread.Sleep(3000)
+            Console.Clear()
+        End If
+
+    End Sub
 
 End Module
